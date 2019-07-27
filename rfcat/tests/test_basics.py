7c4df00366c8dc4b05976e4dc09df14b135f27bf
E R <n644dc@gmail.com>
2019-07-26 23:56:30 -0600

    test

---
+++ rfcat/tests/test_basics.py
@@ -0,0 +1,17 @@
+import rflib
+import unittest
+
+class RfCatBasicTests(unittest.TestCase):
+
+    def test_importing(self):
+        import rflib
+        devs = rflib.getRfCatDevices()
+        self.assertEquals(type(devs), list, "rflib.getRfCatDevices() doesn't return a list!: %r" % devs)
+        import rflib.chipcon_nic
+        import rflib.chipcon_usb
+        import rflib.chipcondefs
+        import rflib.bits
+        import rflib.ccspecan
+        import rflib.intelhex
+        import rflib.rflib_defs
+
